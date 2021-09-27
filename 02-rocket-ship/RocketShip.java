80093c4ad8e74eeff1a9f014b4be3b2c3c43a111
TheCrispBacon <65830367+TheCrispBacon@users.noreply.github.com>
2021-09-27 17:41:17 -0400

    RocketShip Code Assignment, 09/27

---
+++ 02-rocket-ship/RocketShip.java
@@ -0,0 +1,17 @@
+public class RocketShip {
+    public static void main(String[] args) {
+        arrow();
+        shaft();
+        arrow();
+    }// end main method
+
+    public static void arrow() {
+        System.out.println("   /\\ \n  /  \\ \n /    \\");
+    }// end box method
+
+    public static void shaft() {
+        System.out.println("+------+\n|      |\n|      |\n+------+");
+        System.out.println("|United|\n|States|");
+        System.out.println("+------+\n|      |\n|      |\n+------+");
+    }//end shaft method
+}// end class
