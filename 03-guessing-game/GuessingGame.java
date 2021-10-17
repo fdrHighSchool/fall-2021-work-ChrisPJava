2e0b0507c4cb53d8862b6b0ea988d0e8ffd5e5da
ChrisPJava <christopherp836@nycstudents.net>
2021-10-17 09:22:30 -0400

    Guessing Game

--- 03-guessing-game/GuessingGame.java
+++ 03-guessing-game/GuessingGame.java
@@ -3,6 +3,7 @@ import java.util.Random;
 
 public class GuessingGame {
     public static void main(String[] args) {
+        // scanner for main method
         Scanner s = new Scanner(System.in);
 
         // main variables
@@ -12,7 +13,7 @@ public class GuessingGame {
 
         // type of game; 1-10, 1-50, 1-100
         System.out.println("\nWelcome to the number guessing game! You have 3 game mode choices.");
-        System.out.print("Type '1' for numbers 0-10\nType '2' for numbers 1-50\nType '3' for numbers 1-100\nChoice: ");
+        System.out.print("Type '1' for numbers 1-10\nType '2' for numbers 1-50\nType '3' for numbers 1-100\nChoice: ");
         int choice = s.nextInt();
 
         // if statements to set variables for the method w/ choice
@@ -28,23 +29,27 @@ public class GuessingGame {
 
         game(rg,guess,repeat);// runs game method
         s.close();// scanner close
-    }
+    }// end main method
 
     public static void game(int ra, int g, boolean p) {
         int c;
         Scanner s2 = new Scanner(System.in);
         Random r2 = new Random();
 
-        while(p == false) {
+        while(!(p)) {
+            //setup dsplay message
             int msg = r2.nextInt(2);
             System.out.println("\nGuesses left: "+ g);
+            int ranNum = ((r2.nextInt(ra)) + 1);
 
             while(g != 0) {
-                int ranNum = ((r2.nextInt(ra)) + 1);
+                System.out.println("RN:"+ ranNum);
                 System.out.println("Guess a number from 1-"+ ra +"!");
                 c = s2.nextInt();
-
+                
+                //if guess correct
                 if(c == ranNum) {
+                    msg = r2.nextInt(2);
                     //random message for correct guess
                     if(msg == 0) {   
                         System.out.println("Correct!\n");
@@ -53,29 +58,32 @@ public class GuessingGame {
                     else if(msg == 1) {
                         System.out.println("You guessed the right number!\n");
                     }
+                    ranNum = ((r2.nextInt(ra)) + 1);
                 }
 
+                //if guess wrong
                 else {
+                    msg = r2.nextInt(2);
                     //random message for wrong guess
                     if(msg == 0) {
                         g -= 1;  
-                        System.out.println("Incorrect! Guesses left: "+ g +"\n");
+                        System.out.println("Incorrect, try again! Guesses left: "+ g +"\n");
                     }
 
                     else if(msg == 1) {
                         g -= 1;   
-                        System.out.println("Wrong! Guesses left: "+ g +"\n");
+                        System.out.println("Wrong, keep guessing! Guesses left: "+ g +"\n");
                     }
                 }// end while loop
 
             }// end while loop
 
-            // *replay option
+            // **replay option**
             System.out.println("Thank you for playing! Would you like to play again?\nType '1' for yes, type '2' for no.");
             c = s2.nextInt();
 
             if(c == 1) {
-                System.out.print("\nType '1' for numbers 0-10\nType '2' for numbers 1-50\nType '3' for numbers 1-100\nChoice: ");
+                System.out.print("\nType '1' for numbers 1-10\nType '2' for numbers 1-50\nType '3' for numbers 1-100\nChoice: ");
                 c = s2.nextInt();
                 g = 3;
 
@@ -95,8 +103,8 @@ public class GuessingGame {
                 p = true;
                 System.out.println("Thank you for playing!");
             }
-        }
+        }// end while loop
 
         s2.close();// scanner close
-    }// end execute method
-}
+    }// end game method
+}// end class GuessingGame
