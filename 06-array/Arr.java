b6ff4cd12f375f681122e9bdb2c1367541b65b28
ChrisPJava <91324727+ChrisPJava@users.noreply.github.com>
2021-12-12 18:53:55 -0500

    Comment update

--- 06-array/Arr.java
+++ 06-array/Arr.java
@@ -1,15 +1,16 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class Arr {
     public static void main(String[] args) {
-        // Varaible and array gets delcared and initialized
+        // Varaible and arrays gets delcared and initialized
         int arr[] = new int[10]; 
         double arr2[] = new double[100]; 
         Random r = new Random();
         double total = 0.0;
         double rand = 0.0;
         
-        //10 selected values that have a mean of 6
+        //10 selected values that has a mean of 6
         arr[0] = 6;
         arr[1] = 2;
         arr[2] = 3;
@@ -21,10 +22,8 @@ public class Arr {
         arr[8] = 9;
         arr[9] = 10;
 
-        /*
-          For loop traverse through each value of the list and 
-          adds the numbers to total to find the mean
-        */
+        // For loop traverses through each value of the list and adds the numbers to total 
+        
         for(int i = 0; i < arr.length; i++) {
             total = total + arr[i];
         }
@@ -38,7 +37,9 @@ public class Arr {
         
         // Clears previous values
         total = 0.0;
-        // First fl gives each index a random. Second fl value adds them all together into one varaible.
+
+        // First fl gives each index a random
+        // Second fl value adds them all together into one varaible
         for(int i = 0; i < arr2.length; i++) {
             rand = r.nextInt(100);
             arr2[i] = rand;
@@ -53,3 +54,4 @@ public class Arr {
         System.out.println("Average: "+ total +"\n");
     }
 }
+
