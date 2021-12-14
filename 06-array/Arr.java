6ec9522ea628f6646a021e3b5063bff494c6d972
ChrisPJava <91324727+ChrisPJava@users.noreply.github.com>
2021-12-13 21:43:41 -0500

    Code update

--- 06-array/Arr.java
+++ 06-array/Arr.java
@@ -3,55 +3,56 @@ import java.util.Arrays;
 
 public class Arr {
     public static void main(String[] args) {
-        // Varaible and arrays gets delcared and initialized
-        int arr[] = new int[10]; 
-        double arr2[] = new double[100]; 
-        Random r = new Random();
+        int arr[] = {6,2,3,4,5,6,7,8,9,10}; // Mean test case
+        double arr2[] = new double[100]; // Mean w/ random values test case
+        int arr3[] = {7,2,20,5,7,3,2,9,4,6}; // Mode test case
         double total = 0.0;
-        double rand = 0.0;
-        
-        //10 selected values that has a mean of 6
-        arr[0] = 6;
-        arr[1] = 2;
-        arr[2] = 3;
-        arr[3] = 4;
-        arr[4] = 5;
-        arr[5] = 6;
-        arr[6] = 7;
-        arr[7] = 8;
-        arr[8] = 9;
-        arr[9] = 10;
 
         // For loop traverses through each value of the list and adds the numbers to total 
-        
         for(int i = 0; i < arr.length; i++) {
             total = total + arr[i];
         }
 
         // Calculation for mean
-        total = total / arr.length;
-
-        System.out.println("10 values list below.\n");
+        total = total / arr.length; // Mean for arr[]
         System.out.println("Average: "+ total +"\n");
-        System.out.println("100 values list below.\n");
-        
-        // Clears previous values
-        total = 0.0;
+        System.out.println("Average: "+ mean(arr2) +"\n");
+        System.out.println("Average: "+ mode(arr3) +"\n");
+    }
 
+    public static double mean(double[] arr) {
+        Random r = new Random();
+        double total = 0.0;
+        int rand = 0;
         // First fl gives each index a random
-        // Second fl value adds them all together into one varaible
-        for(int i = 0; i < arr2.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             rand = r.nextInt(100);
-            arr2[i] = rand;
+            arr[i] = rand;
+        }
+        // Second fl value adds them all together into one varaible
+        for(int i = 0; i < arr.length; i++) {
+            total = total + arr[i];
         }
 
-        for(int i = 0; i < arr2.length; i++) {
-            total = total + arr2[i];
+        return total / arr.length;
+    }
+
+    public static int mode(int arr[]) {
+        int max = 0;
+        for(int i = 0; i < arr.length-1; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
         }
 
-        // Calculation for mean
-        total = total / arr2.length;
-        System.out.println("Average: "+ total +"\n");
+        int freqArr[] = new int[max];
+
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < freqArr.length; i++) {
+                freqArr[arr[i]] = freqArr[i] + 1;
+            }
+        }
+
+        return 0;
     }
 }
-
