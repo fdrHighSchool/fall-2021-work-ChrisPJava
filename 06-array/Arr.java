21b77542745a481bbcde1348a997285be0977dd0
ChrisPJava <91324727+ChrisPJava@users.noreply.github.com>
2021-12-14 19:32:57 -0500

    Updated mode, max, min, and range functions

--- 06-array/Arr.java
+++ 06-array/Arr.java
@@ -4,23 +4,24 @@ import java.util.Arrays;
 public class Arr {
     public static void main(String[] args) {
         int arr[] = {6,2,3,4,5,6,7,8,9,10}; // Mean test case
+        int arr3[] = {1,1,3,4,5,6,7,8,9,10}; // Mode and Range test case
         double arr2[] = new double[100]; // Mean w/ random values test case
-        int arr3[] = {7,2,20,5,7,3,2,9,4,6}; // Mode test case
         double total = 0.0;
 
         // For loop traverses through each value of the list and adds the numbers to total 
         for(int i = 0; i < arr.length; i++) {
             total = total + arr[i];
-        }
+        }      
 
-        // Calculation for mean
-        total = total / arr.length; // Mean for arr[]
-        System.out.println("Average: "+ total +"\n");
+        // Outputs
+        System.out.println("Average: "+ total / arr.length +"\n");
         System.out.println("Average: "+ mean(arr2) +"\n");
-        System.out.println("Average: "+ mode(arr3) +"\n");
+        System.out.println("Mode: "+ mode(arr3) +"\n");
+        System.out.println("Range: "+ range(arr3) +"\n");
     }
 
     public static double mean(double[] arr) {
+        //Random instance
         Random r = new Random();
         double total = 0.0;
         int rand = 0;
@@ -29,30 +30,69 @@ public class Arr {
             rand = r.nextInt(100);
             arr[i] = rand;
         }
-        // Second fl value adds them all together into one varaible
+        // Second fl adds all values of the array together into one variable
         for(int i = 0; i < arr.length; i++) {
             total = total + arr[i];
         }
 
+        // Returns the mean
         return total / arr.length;
-    }
-
+    }// end mean method
+    
     public static int mode(int arr[]) {
+        // New array storing with the length of the greatest number
+        // The added 2 is for not having 0 to the greatest number
+        int freqArr[] = new int[max(arr)+2];
+
+        // Adds values to the corresponding index of the new array
+        for(int i = 0; i < arr.length; i++) {
+           freqArr[arr[i]]++;
+        }
+
+        // Fl that finds what index has the greatest value and take i 
+        // Because i would used as the value on accessing the indexes of an array
+        int index = 0;
+        for(int i = 0; i < freqArr.length; i++) {
+            if(freqArr[i] == max(freqArr)) {
+                index = i;
+                break; // Breaks once the index of the number that repeats most is found
+            }
+        }  
+
+        // Returns the mode
+        return index;
+    }// end mode method
+
+    public static int range(int[] arr) {
+        return (max(arr) - min(arr));// ># - <#
+    }// end range method
+
+    public static int max(int[] arr) {
         int max = 0;
+        // Fl that compares each index of the array to the max variable
+        // The max variable gets updated if a greater value is found
         for(int i = 0; i < arr.length-1; i++) {
             if(arr[i] > max) {
                 max = arr[i];
             }
         }
+        
+        // Returns the greatest number from the array
+        return max;
+    }// end max method
 
-        int freqArr[] = new int[max];
-
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < freqArr.length; i++) {
-                freqArr[arr[i]] = freqArr[i] + 1;
+    public static int min(int[] arr) {
+        // An extreme value because a number like this wouldn't seen in the arrays given 
+        int min = 999999999;
+        // Fl that compares each index of the array to the max variable
+        // The max variable gets updated if a greater value is found
+        for(int i = 0; i < arr.length-1; i++) {
+            if(arr[i] < min) {
+                min = arr[i];
             }
         }
 
-        return 0;
-    }
-}
+        // Returns the greatest number from the array
+        return min;
+    }// end min method
+}// end class
