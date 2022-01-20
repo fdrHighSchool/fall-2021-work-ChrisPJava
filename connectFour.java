141805f8af0d3648636cf9b6fd52cdf91d68a7c8
ChrisPJava <91324727+ChrisPJava@users.noreply.github.com>
2022-01-19 20:12:47 -0500

    Update connectFour.java

--- connectFour.java
+++ connectFour.java
@@ -5,74 +5,33 @@ public class connectFour {
     Scanner s = new Scanner(System.in);
     String[][] board = new String[6][7];
     boolean cont = true;
-    String row;
-    int row2 = 0;
     String col;
     int col2 = 0;
-    String ltr;
+
+    fillBoard(board);
 
     while(cont) {
-        // Starts with X and then follows O, X, O, X, ...
-        System.out.println("What is your row?");
-        row = s.nextLine();
-        row = row.toLowerCase();
+      // Starts with X and then follows O, X, O, X, ...
+      System.out.println("What is you column?");
+      col = s.nextLine();
+      col = col.toLowerCase();
 
-        if(row.equals("quit")) {
-            System.exit(0);
-            cont = false;
-        }
+      if(col.equals("quit")) {
+        System.exit(0);
+        cont = false;
+      }
 
-        row2 = Integer.parseInt(row);
+      col2 = Integer.parseInt(col);
 
-        System.out.println("What is you column?");
-        col = s.nextLine();
-        col = col.toLowerCase();
-
-        if(col.equals("quit")) {
-            System.exit(0);
-            cont = false;
-        }
-
-        col2 = Integer.parseInt(col);
-
-        System.out.println("What is you letter, X or O?");
-        ltr = s.nextLine();
-        ltr = ltr.toUpperCase();
-
-        if(ltr.equals("quit")) {
-            System.exit(0);
-            cont = false;
-        }
-
-        displayBoard(fillBoard(board, row2, col2, ltr));
+      displayBoard(round(board, col2));
     }
   } // end main method
 
-
-  public static String[][] fillBoard(String[][] board, int r, int c, String l) {
-    r = r - 1;
-    c = c - 1;
+  public static String[][] fillBoard(String[][] board) {
     for(int row = 0; row < board.length; row++) {
       for(int col = 0; col < board[row].length; col++) {
-        if(board[row][col] == "[X]" || board[row][col] == "[O]") {
-            col++;
-        }
-        else {
-            if((row == r) && (col == c)) {
-                // If there is a character in the 
-                if(board[row][col] == "[X]" || board[row][col] == "[O]") {
-                    board[row-1][col] = "["+ l +"]";
-                }
-                else {
-                    board[row][col] = "["+ l +"]";
-                }
-
-                col++;
-                }
-                
-                board[row][col] = "[ ]";
-            } 
-        }   
+        board[row][col] = "[ ]";
+      }   
     }
 
     return board;
@@ -86,6 +45,26 @@ public class connectFour {
       System.out.println();
     } 
   } // end displayBoard method
+
+  public static String[][] round(String[][] board, int c) {
+    String x = "X";
+    String o = "O";
+    int round = 0;
+    c = c - 1;
+
+    for(int row = 0; row < board.length; row++) {
+      for(int col = board[row].length-1; col > 0; col--) {
+        if(row == c) {
+          if(board[row][col] != "[X]" || board[row][col] != "[O]") {
+            board[row][col] = "["+ x +"]";
+          }
+          row++;
+        }
+      } 
+    }
+
+    return board;
+  }// end round method
 } // end class
 
 /*
